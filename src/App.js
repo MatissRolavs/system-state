dc74d324caaddaa4e5d4e8e21168d6269c385e1e
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-16 15:20:34 +0300

    lets go

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import{ useState } from "react";
 function App() {
+
+  const [result, setResult] = useState(0);
+  
+  function addOne() {
+    setResult(result + 1)
+  }
+  function removeOne() {
+    setResult(result - 1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={addOne}>+1</button>
+      <button onClick={removeOne}>-1</button>
+      <h1>{result}</h1>
     </div>
   );
 }
